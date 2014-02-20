09548d89698da9e636839e30b3723ad6bdb8432e
Gino <gino@electricimp.com>
2014-02-19 23:37:15 -0500

    changed to dynamic outgoing port

--- PowerServer.java
+++ PowerServer.java
@@ -1,40 +1,49 @@
 package smartgrid;
 import smartgrid.PowerPacket;
-import java.net.DatagramSocket;
-import java.net.UnknownHostException;
-import java.net.SocketException;
+import java.net.*;
 import java.io.IOException;
 
 public class PowerServer {
+    
+    static final int GRANT_FREQUENCY = 3000;    // How often to send grant packets (milliseconds)
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
+        // TODO: Auto detect broadcast address
+        InetAddress localHost = InetAddress.getLocalHost();
+        NetworkInterface networkInterface = NetworkInterface.getByInetAddress(localHost);
+        for (InterfaceAddress interfaceAddress : networkInterface.getInterfaceAddresses()) {
+            InetAddress broadcastAddr = interfaceAddress.getBroadcast();
+            if (broadcastAddr == null)
+                continue;
+            System.out.println("Broadcast address is " + broadcastAddr.getHostAddress());
+        }
         final String destAddr = "10.0.0.255";
-        final int PORT = 1234;
         int[] authArray = new int[4];
         authArray[0] = 0xDEADBEEF;
         authArray[1] = 0xAABB;
         authArray[2] = 0xCCDDEE;
         authArray[3] = 0xFE;
-        DatagramSocket sendSocket = new DatagramSocket(PORT);
+        // Create new output socket with dynamically assigned port
+        DatagramSocket sendSocket = new DatagramSocket();
         while(true) {
             try {
                 PowerPacket packet = new PowerPacket(destAddr, authArray);
                 sendSocket.send(packet.getPacket());
                 System.out.print('.');
             } catch (UnknownHostException e) {
-                System.err.println("Don't know about host " + destAddr);
+                System.err.println("Unknown host " + destAddr);
                 System.exit(1);
             } catch (SocketException e) {
                 System.err.println("Socket exception " + e.getMessage());
                 System.exit(1);
             }
             try {
-                Thread.sleep(3000);
+                Thread.sleep(GRANT_FREQUENCY);
             } catch (InterruptedException e) {
-                System.out.println("derp");
+                System.out.println("InterruptedException");
             }
         }
     }
